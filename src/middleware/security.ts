99ee01141c1ef488c7b9f4d2067c83a2415bb7b3
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-05-23 18:55:05 +0100

    Add file extensions

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 
-import aj from "../config/arcjet";
+import aj from "../config/arcjet.js";
 import { ArcjetNodeRequest, slidingWindow } from "@arcjet/node";
 
 const securityMiddleware = async (req: Request, res: Response, next: NextFunction) => {
